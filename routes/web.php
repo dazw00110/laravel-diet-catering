8462e38907407567f6ea1fc38e33b568053268a1
Jakub Strzepek <kubas0330@gmail.com>
2025-06-01 22:21:03 +0200

    Utworzenie funkcji roli użytkownika aplikacji, pracownika oraz admina

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,33 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RedirectController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', RedirectController::class)
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth', 'role:2'])->group(function () {
+    Route::get('/client', function () {
+        return view('client.dashboard');
+    })->name('client.dashboard');
+});
+
+Route::middleware(['auth', 'role:3'])->group(function () {
+    Route::get('/staff', function () {
+        return view('staff.dashboard');
+    })->name('staff.dashboard');
+});
+
+Route::middleware(['auth', 'role:1'])->group(function () {
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
